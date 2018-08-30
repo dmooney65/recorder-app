028fc32f4ddee82d8fffe8b7f9f4f3235cfb3c3c
Dominic Mooney <dominic.mooney99@gmail.com>
2018-08-30 14:12:18 +0100

    Disable Settings tab if audio card is busy or errors

--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -7,29 +7,33 @@ router.get('/', (req, res) => {
 
     settingsController.get().then((data) => {
         var card = cardInfo.get(data.captureDevice, cardInfo.CAPTURE);
-        if (card.sampleFormats.includes('S24_LE')) {
-            data.native24Bit = "true";
+        if (card.error) {
+            res.render('settingsError');
         } else {
-            data.native24Bit = "false";
+            if (card.sampleFormats.includes('S24_LE')) {
+                data.native24Bit = "true";
+            } else {
+                data.native24Bit = "false";
+            }
+            var encodeRates = [];
+            if (card.sampleRates.includes(44100)) {
+                encodeRates.push('16/44100');
+            }
+            if (card.sampleRates.includes(48000)) {
+                encodeRates.push('16/48000');
+                encodeRates.push('24/48000');
+            }
+            if (card.sampleRates.includes(88200)) {
+                encodeRates.push('24/88200');
+            }
+            if (card.sampleRates.includes(96000)) {
+                encodeRates.push('24/96000');
+            }
+            if (card.sampleRates.includes(192000)) {
+                encodeRates.push('24/192000');
+            }
+            res.render('settings', { 'settings': data, 'encodeRates': encodeRates });
         }
-        var encodeRates = [];
-        if (card.sampleRates.includes(44100)) {
-            encodeRates.push('16/44100');
-        }
-        if (card.sampleRates.includes(48000)) {
-            encodeRates.push('16/48000');
-            encodeRates.push('24/48000');
-        }
-        if (card.sampleRates.includes(88200)) {
-            encodeRates.push('24/88200');
-        }
-        if (card.sampleRates.includes(96000)) {
-            encodeRates.push('24/96000');
-        }
-        if (card.sampleRates.includes(192000)) {
-            encodeRates.push('24/192000');
-        }
-        res.render('settings', { 'settings': data, 'encodeRates': encodeRates });
     });
 });
 
