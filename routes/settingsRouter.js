37d624c07a59757ba66f5e9a8e7fc6f72f9ed470
Dominic Mooney <dominic.mooney99@gmail.com>
2018-08-30 13:29:59 +0100

    Manage settings based on card capabilities

--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const cardInfo = require('node-alsa-cardinfo');
 const settingsController = require('../src/settingsController.js');
 
 router.get('/', (req, res) => {
 
     settingsController.get().then((data) => {
-        res.render('settings', { 'settings': data });
+        var card = cardInfo.get(data.captureDevice, cardInfo.CAPTURE);
+        if (card.sampleFormats.includes('S24_LE')) {
+            data.native24Bit = "true";
+        } else {
+            data.native24Bit = "false";
+        }
+        var encodeRates = [];
+        if (card.sampleRates.includes(44100)) {
+            encodeRates.push('16/44100');
+        }
+        if (card.sampleRates.includes(48000)) {
+            encodeRates.push('16/48000');
+            encodeRates.push('24/48000');
+        }
+        if (card.sampleRates.includes(88200)) {
+            encodeRates.push('24/88200');
+        }
+        if (card.sampleRates.includes(96000)) {
+            encodeRates.push('24/96000');
+        }
+        if (card.sampleRates.includes(192000)) {
+            encodeRates.push('24/192000');
+        }
+        res.render('settings', { 'settings': data, 'encodeRates': encodeRates });
     });
 });
 
